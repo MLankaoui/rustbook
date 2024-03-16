6d93e718569d1727c433bfed1dbb6525b261eabb
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-16 12:15:11 +0000

    infinite loop feature added

--- 2.guessing_game/guessing_game/src/main.rs
+++ 2.guessing_game/guessing_game/src/main.rs
@@ -14,36 +14,37 @@ fn main() {
 
     println!("{}",secret_number);
 
-    println!("please input your guess");
+    loop {
+        println!("please input your guess");
 
-    //variables in rust are immutable meaning that once declared we can t change them so we use mut keyword to make them mutable, guess a variable assinged to a new instance
-    //of a string String::new , with new is an associated function associated to a datatype
-    
-    //we can seperate a line into multiple lines in rust
-    //stdin is a function provided in io module that will allow us to handle user input properly
-    //if we hadn t do use std::io; at the beginig the syntaxe should look like this std::io::stdin().read_line()
-    io::stdin()
-        //read_line returns Result wich is an enum wich is a type that has some variants in our case Result is used to handle expectation or reading input errors, so Results
-        //has 2 variants , Ok and Err , Ok means operation succeded and err means weencountred an error , and the type of the error or what caused the error etc...
-        .read_line(&mut guess)//readline is a method that takes userinput to stdin by taking the reference to the string & and specifying that the variable is mutable
-        .expect("failed to read user input");// exept is also from Result if the error is in , except will crash the progam and prints the error msg but if ok, the exept
-        //will return the number of bytes you have on the string , if you don t call exept your code will compile but we ll get a warning
+        //variables in rust are immutable meaning that once declared we can t change them so we use mut keyword to make them mutable, guess a variable assinged to a new instance
+        //of a string String::new , with new is an associated function associated to a datatype
+        
+        //we can seperate a line into multiple lines in rust
+        //stdin is a function provided in io module that will allow us to handle user input properly
+        //if we hadn t do use std::io; at the beginig the syntaxe should look like this std::io::stdin().read_line()
+        io::stdin()
+            //read_line returns Result wich is an enum wich is a type that has some variants in our case Result is used to handle expectation or reading input errors, so Results
+            //has 2 variants , Ok and Err , Ok means operation succeded and err means weencountred an error , and the type of the error or what caused the error etc...
+            .read_line(&mut guess)//readline is a method that takes userinput to stdin by taking the reference to the string & and specifying that the variable is mutable
+            .expect("failed to read user input");// exept is also from Result if the error is in , except will crash the progam and prints the error msg but if ok, the exept
+            //will return the number of bytes you have on the string , if you don t call exept your code will compile but we ll get a warning
 
-    //we used shadowing so that we can define the same vaiable more than one time
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    println!("Your guess {guess}");
+        //we used shadowing so that we can define the same vaiable more than one time
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        println!("Your guess {guess}");
 
-        /*
-        let x = 5;
-        let y = 10;
+            /*
+            let x = 5;
+            let y = 10;
 
-        println!("x = {x} and y + 2 = {}", y + 2);
-         */
+            println!("x = {x} and y + 2 = {}", y + 2);
+            */
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
-    }
-
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => println!("You win!"),
+        }
+    }    
 }
