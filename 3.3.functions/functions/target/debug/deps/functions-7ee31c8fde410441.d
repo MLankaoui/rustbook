bce8c4a736cefd81e990cb3eca4aa06247b3adef
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-17 13:26:57 +0000

    function project built

---
+++ 3.3.functions/functions/target/debug/deps/functions-7ee31c8fde410441.d
@@ -0,0 +1,5 @@
+/home/nebula/rust/rustbook/3.3.functions/functions/target/debug/deps/functions-7ee31c8fde410441: src/main.rs
+
+/home/nebula/rust/rustbook/3.3.functions/functions/target/debug/deps/functions-7ee31c8fde410441.d: src/main.rs
+
+src/main.rs:
---
+++ 3.3.functions/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/nebula/rust/rustbook/3.3.functions/functions/target/debug/functions: /home/nebula/rust/rustbook/3.3.functions/functions/src/main.rs
