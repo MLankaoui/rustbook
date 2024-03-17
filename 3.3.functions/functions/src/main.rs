45faff7dab76283530d62595e2d450f9a4e31d15
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-17 13:33:47 +0000

    how to call a function and to use parameters

--- 3.3.functions/functions/src/main.rs
+++ 3.3.functions/functions/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    another_function(5);
+}
+//it doesn t matter where to put this function as long as their defined somewhere in a scope that can be seen by the caller
+fn another_function(x: u32) {
+    println!("Another function.");
+    println!("The value of x is: {x}");
 }
