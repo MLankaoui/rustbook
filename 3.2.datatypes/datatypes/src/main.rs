8d74bbbd8a8ea0d0224853a70d2dc7aa693ea9b9
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-17 12:18:38 +0000

    done with tuples

--- 3.2.datatypes/datatypes/src/main.rs
+++ 3.2.datatypes/datatypes/src/main.rs
@@ -69,4 +69,14 @@ fn main() {
     let (x, y, z) = tup;
 
     println!("The value of y is : {y}")
+
+    //we can easily access a value in a tuple using "."
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+
+    let six_point_four = x.1;
+
+    let one = x.2;
 }
