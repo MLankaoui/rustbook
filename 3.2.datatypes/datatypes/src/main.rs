8deadd98cda5eb71c67439b5decd574f2e8e3a18
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-17 12:09:16 +0000

    rust maths operations

--- 3.2.datatypes/datatypes/src/main.rs
+++ 3.2.datatypes/datatypes/src/main.rs
@@ -26,5 +26,24 @@ fn main() {
     usigned integer range = 0 to 2^n - 1 if u8 0 to 2^8 - 1
     */
 
-    
+    //floatings: f32, f64 by default all floating values are of type f64
+    let x = 2.0// f64 has double precision
+    let y: f32 = 3.0 // f32 single precision
+
+    //maths operations in rust
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
 }
