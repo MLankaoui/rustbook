92d79f2bf0d8714dda1e553655c4b9f92cf0eeb0
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-17 13:39:11 +0000

    two arguments function

--- 3.3.functions/functions/src/main.rs
+++ 3.3.functions/functions/src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
     println!("Hello, world!");
     another_function(5);
+    print_labeled_measurements(5, 'h');
 }
 //it doesn t matter where to put this function as long as their defined somewhere in a scope that can be seen by the caller
-fn another_function(x: u32) {
+fn another_function(x: i32) {
     println!("Another function.");
     println!("The value of x is: {x}");
+}
+// a function that has two arguments
+fn print_labeled_measurements(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
 }
