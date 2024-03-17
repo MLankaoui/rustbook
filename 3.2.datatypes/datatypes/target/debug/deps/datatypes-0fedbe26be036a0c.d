7a54719bbc4de7ddaa4269bf487246aa4597d3c7
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-17 11:44:35 +0000

    performed cargo check on the part 3.2

---
+++ 3.2.datatypes/datatypes/target/debug/deps/datatypes-0fedbe26be036a0c.d
@@ -0,0 +1,5 @@
+/home/nebula/rust/rustbook/3.2.datatypes/datatypes/target/debug/deps/libdatatypes-0fedbe26be036a0c.rmeta: src/main.rs
+
+/home/nebula/rust/rustbook/3.2.datatypes/datatypes/target/debug/deps/datatypes-0fedbe26be036a0c.d: src/main.rs
+
+src/main.rs:
