d505bc0b64b5c576fc47851caa0d4a82fe61e369
MLankaoui <marouanelankaoui66@gmail.com>
2024-03-20 13:49:11 +0000

    understanded scope

--- section_four/ownership/src/main.rs
+++ section_four/ownership/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    //ownership rules
+    /*
+    each value in rust has an owner
+    there can only be one owner at a time
+    when the owner goes out of scope, the value will be dropped
+     */
+    {
+        let s = "hello";// s is just valid in this scope, it remains valid untill it gors out of scope
+    }
 }
